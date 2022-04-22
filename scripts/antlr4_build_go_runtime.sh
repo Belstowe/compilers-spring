c1ce90327c3a610e946a843f5ae4d32a236bd324
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-04-22 20:04:38 +0700

    Подправлены скрипты Корректная установка antlr4 go runtime В выводе curl теперь указан конкретный файл

--- scripts/antlr4_build_go_runtime.sh
+++ scripts/antlr4_build_go_runtime.sh
@@ -2,6 +2,6 @@
 
 readonly antlr_ver=4.10.1
 
-go install github.com/antlr/antlr4/runtime/Go/antlr@$antlr_ver
+go get -d github.com/antlr/antlr4/runtime/Go/antlr@$antlr_ver
 
 java org.antlr.v4.Tool -Dlanguage=Go build/grammar/RustLexer.g4
--- scripts/install_antlr4.sh
+++ scripts/install_antlr4.sh
@@ -3,5 +3,5 @@
 readonly antlr=antlr-4.10.1-complete.jar
 readonly workdir=~/.local/share/applications
 
-curl -O https://www.antlr.org/download/$antlr -o $workdir
+curl -O https://www.antlr.org/download/$antlr -o $workdir/$antlr
 export CLASSPATH=".:$workdir/$antlr:$CLASSPATH"
